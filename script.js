98cb6fadb54fbb19fd4a6a03da118e8881895de4
Felipegithubf <felipecarvalho5055@gmail.com>
2024-06-11 07:57:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,143 @@
+var animation = true;
+if(animation == true){
+    var sr = ScrollReveal({reset: true});
+
+    sr.reveal('h1', {duration : 1000});
+
+    sr.reveal('span', {
+        rotate: { x: 0, y: 80, z: 0 },
+        duration: 2000,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('h2', {
+        rotate: { x: 0, y: 100, z: 0 },
+        duration: 2000,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('em', {
+        rotate: { x: 0, y: 100, z: 0 },
+        duration: 2000,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('.area1--text', {
+        rotate: { x: 0, y: 100, z: 0 },
+        duration: 2000,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('.area2--text', {
+        rotate: { x: 0, y: 100, z: 0 },
+        duration: 2000,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('p', {
+        rotate: { x: 0, y: 100, z: 0 },
+        duration: 2000,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('.area4--text', {
+        rotate: { x: 0, y: 100, z: 0 },
+        duration: 2000,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('a', {
+        rotate: { x: 0, y: 100, z: 0 },
+        duration: 2500,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('.banner', {
+        rotate: { x: 0, y: 20, z: 0 },
+        duration: 1800,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('.area1--img2', {
+        rotate: { x: 0, y: 10, z: 0 },
+        duration: 1800,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('.banner--images', {
+        rotate: { x: 0, y: 500, z: 0 },
+        duration: 1800,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('.area5', {
+        rotate: { x: 0, y: 10, z: 0 },
+        duration: 1800,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('.area3--img1', {
+        rotate: { x: 0, y: 10, z: 0 },
+        duration: 1800,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('.bar', {
+        rotate: { x: 0, y: 10, z: 0 },
+        duration: 1800,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('.area4--img1', {
+        rotate: { x: 0, y: 10, z: 0 },
+        duration: 1800,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('.area2--img1', {
+        rotate: { x: 0, y: 10, z: 0 },
+        duration: 1800,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('.area2--img2', {
+        rotate: { x: 0, y: 10, z: 0 },
+        duration: 1800,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('img', {
+        rotate: { x: 0, y: 10, z: 0 },
+        duration: 1800,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('.area1--img1', {
+        rotate: { x: 0, y: 10, z: 0 },
+        duration: 1800,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('placeholder', {
+        rotate: { x: 0, y: 10, z: 0 },
+        duration: 1800,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('input', {
+        rotate: { x: 0, y: 10, z: 0 },
+        duration: 1800,
+        ease: 'power2.out'
+    });
+
+    sr.reveal('.line', {
+        rotate: { x: 0, y: 10, z: 0 },
+        duration: 1800,
+        ease: 'power2.out'
+    });
+}
+
+
+
+
+
